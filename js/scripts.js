ba19feb98c01d5db11414829b147b2d06e570cee
Adam McFadden & Dan Minor <student@epicodus.com>
2015-06-01 11:55:53 -0700

    write form for location, landmarks, time of year, notes. displays list of places. each place displays it's attributes.

---
+++ js/scripts.js
@@ -0,0 +1,41 @@
+$(document).ready(function(){
+  $("#new-landmark").click(function() {
+    $("#landmarks").append('<div class="landmarks">' +
+                                 '<div class="form-group">' +
+                                    '<label for="landmarks"><b>Landmarks:</b></label>' +
+                                    '<input required id="landmarks" type="text" class="form-control landmarks">' +
+                                 '</div>' +
+                               '</div>');
+  });
+
+  $("form#places").submit(function(event) {
+    event.preventDefault();
+
+    var inputtedLocation = $("input#location").val();
+    var inputtedTimeOfYear = $("select#time-of-year").val();
+    var inputtedNotes = $("textarea#Notes").val();
+    var newPlace = {place: inputtedLocation, landmarks: [], timeOfYear: inputtedTimeOfYear, notes: inputtedNotes};
+    $(".landmarks").each(function() {
+      var inputtedLandmark = $(this).find("input#landmarks").val();
+      newPlace.landmarks.push(inputtedLandmark);
+    });
+
+    $("ul#place-results").append("<li><span class='place'>" + newPlace.place + "</span></li>");
+
+    $(".place").last().click(function() {
+      $(".show-place").show();
+      $(".show-place h3").text(newPlace.place);
+      $("#time-of-year-results").text(newPlace.timeOfYear);
+      $("ul#landmark-results").text("");
+      newPlace.landmarks.forEach(function(landmark) {
+        $("ul#landmark-results").append("<li>" + landmark + "</li>");
+      });
+      $("#notes-results").text(newPlace.notes)
+      $("input").val("");
+    })
+    // $("ul#landmark-results").append("<li><span class='place'>" + newPlace.location )
+
+
+  $("#result").show();
+  });
+});
